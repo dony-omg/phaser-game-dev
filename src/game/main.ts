fccb5f3268a8420ae33b75713ba200166b771c47
Trung Tran <trung.tran@kyanon.digital>
2026-02-05 16:31:37 +0700

    fix layout respotive

--- src/game/main.ts
+++ src/game/main.ts
@@ -2,17 +2,21 @@ import { Boot } from './scenes/Boot';
 import { GameOver } from './scenes/GameOver';
 import { Game as MainGame } from './scenes/Game';
 import { MainMenu } from './scenes/MainMenu';
-import { AUTO, Game } from 'phaser';
+import Phaser, { AUTO, Game } from 'phaser';
 import { Preloader } from './scenes/Preloader';
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Phaser.Types.Core.GameConfig = {
     type: AUTO,
-    width: 720,
-    height: 1280,
     parent: 'game-container',
     backgroundColor: '#3fb3e8',
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 720,
+        height: 1280
+    },
     scene: [
         Boot,
         Preloader,
